8ea3db833f166f41e240505724a2f9689c236196
Maze Winther <mazewinther@gmail.com>
2026-02-01 16:43:57 +0100

    organize imports

--- apps/web/src/services/storage/migrations/index.ts
+++ apps/web/src/services/storage/migrations/index.ts
@@ -1,9 +1,9 @@
 export { StorageMigration } from "./base";
 export { StorageVersionManager } from "./version-manager";
-export { runStorageMigrations } from "./runner";
 import { V0toV1Migration } from "./v0-to-v1";
 import { V1toV2Migration } from "./v1-to-v2";
 import { V2toV3Migration } from "./v2-to-v3";
+export { runStorageMigrations } from "./runner";
 
 export const CURRENT_STORAGE_VERSION = 3;
 
